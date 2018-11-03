738da8021e09b86ec390970d7a943e22b1551654
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2018-11-02 23:18:53 -0400

    Add reloading indicator to city list and refactor city list view controller

--- Backbase-CityFinder/Controllers/CityListViewController.swift
+++ Backbase-CityFinder/Controllers/CityListViewController.swift
@@ -12,15 +12,24 @@ class CityListViewController: UITableViewController {
     
     private static var reuseIdentifier = "cityListTableViewCell"
 
+    // MARK: - Properties
+    
     private var fileProvider: FileProvider
     
-    private var cities = [CityViewModel]()
+    private var cityViewModels = [CityViewModel]()
+    
+    // MARK: - Subviews
+    
+    private var loadingActivityIndicatorView: UIActivityIndicatorView = {
+        let indicatorView = UIActivityIndicatorView(style: .gray)
+        indicatorView.hidesWhenStopped = true
+        return indicatorView
+    }()
     
     // MARK: - Initializer
     
     init(fileProvider: FileProvider) {
         self.fileProvider = fileProvider
-        
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -35,45 +44,75 @@ class CityListViewController: UITableViewController {
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: CityListViewController.reuseIdentifier)
         
+        setupNavigationBar()
+        fetchData()
+    }
+    
+    // MARK: - Convenience
+    
+    private func setupNavigationBar() {
         title = "City Finder"
         if #available(iOS 11.0, *) {
             navigationController?.navigationBar.prefersLargeTitles = true
         }
         
         navigationController?.isToolbarHidden = false
-        
-        do {
-            guard let data = try fileProvider.contents() else {
-                fatalError("Data not found.")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: loadingActivityIndicatorView)
+    }
+    
+    private func fetchData() {
+        loadingActivityIndicatorView.startAnimating()
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                guard let data = try self.fileProvider.contents() else {
+                    DispatchQueue.main.async {
+                        self.displayError(message: "Data not found.")
+                    }
+                    
+                    return
+                }
+                
+                let decodedCities = try JSONDecoder().decode([City].self, from: data).sorted { $0.name < $1.name }
+                self.cityViewModels = decodedCities.map { CityViewModel(city: $0) }
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.loadingActivityIndicatorView.stopAnimating()
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.displayError(message: error.localizedDescription)
+                }
             }
-            
-            let decodedCities = try JSONDecoder().decode([City].self, from: data).sorted { $0.name < $1.name }
-            cities = decodedCities.map { CityViewModel(city: $0) }
-            tableView.reloadData()
-        } catch {
-            print(error.localizedDescription)
         }
+    }
+    
+    private func displayError(message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        alertController.addAction(dismissAction)
+        present(alertController, animated: true)
     }
     
     // MARK: - UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedCity = cities[indexPath.row]
-        
+        let selectedCity = cityViewModels[indexPath.row]
         let mapViewController = MapViewController(city: selectedCity)
+        
         navigationController?.pushViewController(mapViewController, animated: true)
     }
     
     // MARK: - UITableViewDataSource
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cities.count
+        return cityViewModels.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CityListViewController.reuseIdentifier, for: indexPath)
         
-        cell.textLabel?.text = cities[indexPath.row].displayName
+        cell.textLabel?.text = cityViewModels[indexPath.row].displayName
         
         return cell
     }
